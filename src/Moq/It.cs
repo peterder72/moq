3569b288db0ef9b8cd3af6fb1fbac70f5fbb1c3e
Petr Deriabin <peterder72@osetr.su>
2023-09-09 22:02:25 +0200

    Made It.Is match expression compilation only happen once

--- src/Moq/It.cs
+++ src/Moq/It.cs
@@ -165,9 +165,10 @@ namespace Moq
             }
 
             var thisMethod = (MethodInfo)MethodBase.GetCurrentMethod();
+            var compiledMatchMethod = match.CompileUsingExpressionCompiler();
 
             return Match.Create<TValue>(
-                argument => match.CompileUsingExpressionCompiler().Invoke(argument),
+                argument => compiledMatchMethod.Invoke(argument),
                 Expression.Lambda<Func<TValue>>(Expression.Call(thisMethod.MakeGenericMethod(typeof(TValue)), match)));
         }
 
@@ -188,9 +189,10 @@ namespace Moq
         public static TValue Is<TValue>(Expression<Func<object, Type, bool>> match)
         {
             var thisMethod = (MethodInfo)MethodBase.GetCurrentMethod();
+            var compiledMatchMethod = match.CompileUsingExpressionCompiler();
 
             return Match.Create<TValue>(
-                (argument, parameterType) => match.CompileUsingExpressionCompiler().Invoke(argument, parameterType),
+                (argument, parameterType) => compiledMatchMethod.Invoke(argument, parameterType),
                 Expression.Lambda<Func<TValue>>(Expression.Call(thisMethod.MakeGenericMethod(typeof(TValue)), match)));
         }
 
